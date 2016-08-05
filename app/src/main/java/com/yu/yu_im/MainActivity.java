c5a8829612e46b9d8e588af04a8443b039fdeaf4
Yu <zsyzsy1818@hotmail.com>
2016-08-05 12:13:21 +0800

    Initial commit

---
+++ app/src/main/java/com/yu/yu_im/MainActivity.java
@@ -0,0 +1,13 @@
+package com.yu.yu_im;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
